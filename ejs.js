59eac4e965cb69697fcd0b15b83f8aef0763a444
zigzga <447587073@qq.com>
2020-01-31 22:42:31 +0800

    add mysql

--- ejs.js
+++ ejs.js
@@ -1,20 +1,41 @@
 const Koa = require('koa');
 const views = require('koa-views');
 const path = require('path');
+const mysql = require('mysql');
+
 const app = new Koa();
 
 // 加载模板引擎
 app.use(
-    views(path.join(__dirname, './view'), {
+    views(path.join(__dirname, './ejs'), {
         extension: 'ejs'
     })
 );
 
-app.use(async ctx => {
-    let title = 'hello koa2';
-    await ctx.render('index', {
-        title
+// 连接数据库
+const connection = mysql.createConnection({
+    host: '127.0.0.1', // 数据库地址
+    user: 'root', // 数据库用户
+    password: '123456', // 数据库密码
+    database: 'hellothinkjs' // 选中数据库
+});
+
+let title = 'hello 404';
+let users = [];
+
+connection.connect();
+connection.query('SELECT * FROM think_user', async (error, results, fields) => {
+    if (error) throw error;
+    // connected !
+    console.log(results);
+    users = results;
+    app.use(async ctx => {
+        await ctx.render('404', {
+            title,
+            users
+        });
     });
 });
+connection.end();
 
 app.listen(3000);
