722fae4b2caa5050aa3a82a39291c0b7099b0525
zigzga <447587073@qq.com>
2020-02-01 01:06:56 +0800

    add new test haha

--- test/index.test.js
+++ test/index.test.js
@@ -6,11 +6,41 @@ const expect = chai.expect;
 const request = supertest(app.listen());
 
 // 测试套件/组
-describe('开始测试demo的GET请求', () => {
-    // 测试用例
-    it('测试/getString.json请求', done => {
+describe('开始测试什么中药可以抑制冠状病毒肺炎', () => {
+    // 测试用例1
+    it('测试板蓝根可以制冠状病毒肺炎', done => {
         request
             .get('/getString.json')
+            .expect(200)
+            .end((err, res) => {
+                // 断言判断结果是否为object类型
+                expect(res.body).to.be.an('object');
+                expect(res.body.success).to.be.an('boolean');
+                expect(res.body.data).to.be.an('number');
+                done();
+            });
+    });
+    // 测试用例 2
+    it('测试双黄连口服液可以制冠状病毒肺炎', done => {
+        request
+            .get('/getNumber.json')
+            .expect(200)
+            .end((err, res) => {
+                // 断言判断结果是否为object类型
+                expect(res.body).to.be.an('object');
+                expect(res.body.success).to.be.an('boolean');
+                expect(res.body.data).to.be.an('string');
+                done();
+            });
+    });
+});
+
+// 测试套件/组
+describe('开始测试智商税了', () => {
+    // 测试用例
+    it('测试你的智商是不是二百五', done => {
+        request
+            .post('/postData.json')
             .expect(200)
             .end((err, res) => {
                 // 断言判断结果是否为object类型
